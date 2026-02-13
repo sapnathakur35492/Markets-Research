6c65d2dc89fd76a257af9c7a61abeceef22ab8f9
priyankush <pksalo6@gmail.com>
2026-02-13 17:04:59 +0530

    fixed admin issue

--- market_research_backend/settings.py
+++ market_research_backend/settings.py
@@ -42,6 +42,11 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS', 
+    'https://marketsnxt.com'
+).split(',')
+
 
 # Application definition
 
